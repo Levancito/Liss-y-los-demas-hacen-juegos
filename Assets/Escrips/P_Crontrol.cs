79ea0b41be46b61ef2a40fc8da532b8332d3883c
Levancito <valentin.diaz@davinci.edu.ar>
2024-09-10 21:29:01 -0300

    When haces tus momos en github :v

--- Assets/Escrips/P_Crontrol.cs
+++ Assets/Escrips/P_Crontrol.cs
@@ -7,10 +7,34 @@ public class P_Crontrol : MonoBehaviour
 {
     [SerializeField] P_Controller _controller;
     [SerializeField] float _speed;
+    [SerializeField] float _max = 0.1f;
+
+    private Camera _camera;
+    private float _screenWidth;
+    private float _screenHeight;
+
+    void Start()
+    {
+        _camera = Camera.main;
+        _screenWidth = _camera.orthographicSize * _camera.aspect;
+        _screenHeight = _camera.orthographicSize;
+    }
 
     void Update()
     {
-        transform.position += _controller.GetMovementInput() * _speed * Time.deltaTime;
+        Vector3 moveInput = _controller.GetMovementInput() * _speed * Time.deltaTime;
+        Vector3 newPosition = transform.position + moveInput;
+
+
+        float xMin = -_screenWidth + _max;
+        float xMax = _screenWidth - _max;
+        float yMin = -_screenHeight + _max;
+        float yMax = _screenHeight - _max;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+        newPosition.y = Mathf.Clamp(newPosition.y, yMin, yMax);
+
+        transform.position = newPosition;
     }
 
 }
